e8d18b1233735ceca39692587cc527bdfa8296d3
Till Hoffmann <tillahoffmann@gmail.com>
2025-02-06 16:16:49 -0500

    Verify `skip_if_traced` behavior for `accumulate_on_update`.

--- optinspect/tests/test_accumulate.py
+++ optinspect/tests/test_accumulate.py
@@ -1,3 +1,4 @@
+import jax
 from jax import numpy as jnp
 import optinspect
 import pytest
@@ -12,18 +13,35 @@ from typing import Any, Callable
         (optinspect.accumulate_most_recent(lambda **kwargs: kwargs["state"].count), 9),
     ],
 )
-def test_accumulate(accumulate_func: Callable, expected: Any) -> None:
-    x = jnp.array(4.0)
-    optim = optinspect.accumulate_on_update(accumulate_func)
+@pytest.mark.parametrize("scan", [False, True], ids=["loop", "scan"])
+def test_accumulate(
+    accumulate_func: Callable, expected: Any, skip_if_traced: bool, scan: bool
+) -> None:
+    n = 10
+    x = 4
+    optim = optinspect.accumulate_on_update(
+        accumulate_func, skip_if_traced=skip_if_traced
+    )
     state = optim.init(x)
 
-    n = 10
-    for grad in jnp.arange(n):
-        updated, state = optim.update(grad, state, x)
-        assert jnp.allclose(updated, grad), "Gradient must be unchanged."
-
-    assert state.count == n
-    assert jnp.allclose(state.value, expected)
+    if scan:
+        (updated, state), _ = jax.lax.scan(
+            lambda carry, grad: (optim.update(grad, carry[1], x), None),
+            (x, state),
+            jnp.arange(n),
+        )
+        if skip_if_traced:
+            assert jnp.allclose(state.value, x)
+            assert state.count == 0
+        else:
+            assert jnp.allclose(state.value, expected)
+            assert state.count == n
+    else:
+        for grad in jnp.arange(n):
+            updated, state = optim.update(grad, state, x)
+            assert jnp.allclose(updated, grad), "Gradient must be unchanged."
+        assert state.count == n
+        assert jnp.allclose(state.value, expected)
 
 
 def test_accumulate_invalid_key() -> None:
